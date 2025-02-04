7adc8be4d28336e7cd634075e0c9c589c9b026ba
rsjspellen <rons2790@nycstudents.net>
2025-02-04 14:54:30 +0000

    Changes

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -19,7 +19,7 @@ name = "tanji";
 age = 39;
 age = 40;
 age = 50;
-alert(age);
+// alert(age);
 
 // Essential Operations
 
@@ -39,6 +39,104 @@ alert(age);
 // Conditional statements;
 
 
+
+// Functions
+
+// Returns half the number if it can be halved; otherwise returns false
+
+
+// Functions can also be written as arrows. Body of function doesn't change, just the signature.
+
+// Fundamental data types
+
+/*
+Numbers - (integers, floats)
+Strings
+Booleans
+objects - (arrays, objects)
+*/
+
+// T
+// // Typeof
+// alert(typeof "" ); 
+// alert(typeof 1 );
+// alert(typeof true );
+// alert(typeof null );
+// B
+
+// Let vs Const vs Var
+let x = 1;
+const y = 2;
+var z = 3;
+
+// y = 3;
+
+// Reassignment (no let)
+age = 45;
+age = 50;
+age = 55;
+
+// alert(age);
+
+// T
+// // Essential Operations
+// let sum = age + 5;
+// console.log(sum);
+
+// sum = sum - 5
+// sum -= 5
+// console.log(sum);
+
+// sum = sum * 2
+// sum *= 2
+// console.log(sum);
+
+// sum = sum / 2 
+// sum /= 2
+// console.log(sum);
+
+// sum = sum + 2  
+// sum += 2
+// console.log(sum);
+// B
+
+
+// Additional operations
+
+// T
+// // User Input
+// let friend = prompt("Who is your friend? ");
+
+// // Method 1:
+// console.log("Your best friend is " + friend);
+
+// // Method 2:
+// console.log("Your best friend is ",friend);
+
+// // Method 3:
+// console.log(`Your best friend is ${friend}`);
+// B
+
+// Concatenation v. templates
+
+let statement1 = "lil pump better";
+let statement2 = " i might swerve bend that corner whooo ooo oooh";
+let statement3 = "i might swerve bend that corner whooo ooo oooh";
+
+console.log(statement1 == statement2);
+// String methods
+
+const name = "LIL PUMP";
+console.log(statement2.toUpperCase());
+console.log(name.toLowerCase());
+// Comparison operators - true or false?
+
+
+// Conditional statements;
+
+// Math methods
+console.log(Math.random()*150);
+console.log(Math.floor(6.3));
 
 // Functions
 
